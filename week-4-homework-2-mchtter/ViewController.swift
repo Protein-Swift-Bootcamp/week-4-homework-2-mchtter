dba2ddc55df0f04f57f8bb64920d38012e6f87e4
Alperen ERYILMAZ <mchtter@gmail.com>
2022-12-31 21:01:16 +0300

    xcode init

---
+++ week-4-homework-2-mchtter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-4-homework-2-mchtter
+//
+//  Created by Mücahit Alperen Eryılmaz on 31.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
